9b75ad6bf908fed59d92c39ee65f81dd61689dd3
Roman Cherepanov <romach007@gmail.com>
2025-05-02 15:27:34 +0300

    update posts

--- how-to-test-js-app-with-mocha-and-chai/index.test.js
+++ how-to-test-js-app-with-mocha-and-chai/index.test.js
@@ -3,6 +3,7 @@ const { getHelloMessage } = require('./index');
 const assert = require('assert');
 
 describe('getHelloMessage', () => {
+
   it('should return "Hello, world!"', () => {
     const actualResult = getHelloMessage();
     assert.equal('Hello, world!', actualResult);
